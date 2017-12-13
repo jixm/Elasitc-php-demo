bdde39278157a2f6c7a1c3a99ed1fe20e43c0010
纪晓明 <ji.xiaoming@scimall.org.cn>
2017-12-13 09:42:04 +0800

    fix

--- example/Config/Mapping.php
+++ example/Config/Mapping.php
@@ -6,5 +6,43 @@
  */
 namespace Config;
 class Mapping {
-    
+    public static $test_v1 = array(
+		 'index' => 'test_v1',
+        'body' => [
+            'settings' => [
+                'number_of_shards' => 1,
+                'number_of_replicas' => 0,
+                'analysis' => [
+                    "analyzer" => [
+                        "ik" => [
+                            "tokenizer" => "ik_max_word"
+                        ]
+                    ]
+                ]
+            ],
+            'mappings' => [
+                'test' => [
+                    'properties' => [
+                        'content' =>[
+                            "type"=> "integer",
+                            'index' => 'not_analyzed'
+                        ],
+                        'title' =>[
+                            "type"=> "text",
+                            "analyzer"=> "ik_max_word",
+                        ],
+                        'description' =>[
+                            "type"=> "text",
+                            "analyzer"=> "ik_max_word",
+                        ],
+                        'create_time'  => [
+                            'type' => 'integer',
+                            'index' => 'not_analyzed'
+                        ],
+                    ]
+                ]
+            ]
+        ]
+
+    );
 }
