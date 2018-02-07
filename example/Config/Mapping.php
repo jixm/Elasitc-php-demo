b97e878e2de293a8aae0adb2a47e4ccc0ba13cc2
纪晓明 <ji.xiaoming@scimall.org.cn>
2018-02-07 11:06:44 +0800

    mapping

--- example/Config/Mapping.php
+++ example/Config/Mapping.php
@@ -6,43 +6,63 @@
  */
 namespace Config;
 class Mapping {
-    public static $test_v1 = array(
-		 'index' => 'test_v1',
+    public static $user = [
+        'index' => 'user',
         'body' => [
             'settings' => [
                 'number_of_shards' => 1,
                 'number_of_replicas' => 0,
                 'analysis' => [
-                    "analyzer" => [
-                        "ik" => [
-                            "tokenizer" => "ik_max_word"
+                    'filter' => [
+                        'user_pinyin' => [
+                            'type' => 'pinyin',
+                            'firs_letter' => 'prefix',
+                            'padding_char' => ' '
+                        ]
+                    ],
+                    'analyzer' => [
+                        'ik_pinyin_analyzer' => [
+                            'type' => 'custom',
+                            'tokenizer' => 'ik_smart',
+                            'filter' => ['user_pinyin','word_delimiter']
                         ]
                     ]
                 ]
             ],
             'mappings' => [
-                'test' => [
+                'list' => [
                     'properties' => [
-                        'content' =>[
-                            "type"=> "integer",
-                            'index' => 'not_analyzed'
+                        'name' =>[
+                            "type"=> "keyword",
+                            "fields"=> [
+                                "pinyin"=> [
+                                    "type"=> "text",
+                                    "store"=> "yes",
+                                    "term_vector"=> "with_positions_offsets",
+                                    "analyzer"=> "ik_pinyin_analyzer",
+                                ],
+                                "cn" =>  [
+                                    "type"=> "text",
+                                    "store"=> "yes",
+                                    "term_vector"=> "with_positions_offsets",
+                                    "analyzer"=> "ik_max_word",
+                                ],
+                            ]
                         ],
-                        'title' =>[
-                            "type"=> "text",
-                            "analyzer"=> "ik_max_word",
+                       
+                        'id'  => [
+                            'type' => 'long',
+                            'index'=> 'not_analyzed',
                         ],
-                        'description' =>[
-                            "type"=> "text",
-                            "analyzer"=> "ik_max_word",
-                        ],
-                        'create_time'  => [
-                            'type' => 'integer',
+                        'create_time' => [
+                            'type' => 'date',
+                            "store"=> "yes",
+                            'format' => 'yyyy-MM-dd HH:mm:ss',
                             'index' => 'not_analyzed'
                         ],
                     ]
-                ]
+                ],
             ]
         ]
-
-    );
+    ];
 }
